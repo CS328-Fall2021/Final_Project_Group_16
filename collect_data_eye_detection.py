08513d5f5c02217591fcf0002a2db3c1d77c40be
T'aldarim <zc470618625@gmail.com>
2021-12-02 21:16:29 -0500

    data collection file

--- collect_data_eye_detection.py
+++ collect_data_eye_detection.py
@@ -1,13 +1,12 @@
-import sys
-import json
 import os
 import cv2 as cv
 import numpy as np
-import dlib
+import imutils
 
 
-# class_labels = ['blinking', 'eye open', 'eye close', 'frown']
-# labels_index = [    0     ,      1    ,       2    ,    3   ]
+# class_labels = ['blinking', 'eye open', 'frown']
+# labels_index = [    0     ,      1    ,    2   ]
+
 label = 0
 
 filename="eye-data-Kai.csv"#"eye-data-HenryVIII-1.csv"
@@ -20,25 +19,42 @@ if not os.path.exists(data_dir):
 
 cap = cv.VideoCapture(0)
 
-while True:
-    try:
-        _, frame = cap.read()
-        # each frame is a np array with shape 480*640*3
-        
+labelled_data = []
 
+try:
+    while True:
+        try:
+            _, frame = cap.read()
+            # each frame is a np array with shape 480*640*3
+            frame = imutils.resize(frame, width=640)
+            cv.imshow('Video_small', frame)
+            raveled = np.append(frame.ravel(),label)
+            # each row would contain (921601,) which 480*640*3 video elements + 1 label 
+            labelled_data.append(raveled)
 
-    except KeyboardInterrupt: 
-            # occurs when the user presses Ctrl-C
+            if cv.waitKey(1) == ord('q'):
+                raise KeyboardInterrupt
+
+        except KeyboardInterrupt:
             print("User Interrupt. Saving labelled data...")
             labelled_data = np.asarray(labelled_data)
-            with open(os.path.join(data_dir, filename), "wb") as f:
-                np.savetxt(f, labelled_data, delimiter=",")
+            with open(os.path.join(data_dir, filename), "wb") as doc:
+                np.savetxt(doc, labelled_data, delimiter=",")
             break
 
-    except Exception as e:
-        # ignore exceptions, such as parsing the json
-        # if a connection timeout occurs, also ignore and try again. Use Ctrl-C to stop
-        # but make sure the error is displayed so we know what's going on
-        if (str(e) != "timed out"):  # ignore timeout exceptions completely       
-            print(e)
-        pass
+        except Exception as e:
+            # ignore exceptions, such as parsing the json
+            # if a connection timeout occurs, also ignore and try again. Use Ctrl-C to stop
+            # but make sure the error is displayed so we know what's going on
+            if (str(e) != "timed out"):  # ignore timeout exceptions completely       
+                print(e)
+            pass
+except KeyboardInterrupt: 
+    # occurs when the user presses Ctrl-C
+    print("User Interrupt. Quitting...")
+    quit()
+
+finally:
+    print('Closing Video Data Capturing')
+    cap.release()
+    cv.destroyAllWindows()
